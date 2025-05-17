a8c9ca8dfe61ffc5bead88d7cdb4ee7860d76036
saurabhdutta047 <saurabhdutta047@gmail.com>
2025-05-17 18:45:45 +0530

    feat: First commit

---
+++ MenuPanda/MenuPandaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuPandaApp.swift
+//  MenuPanda
+//
+//  Created by Saurabh Dutta on 17/05/25.
+//
+
+import SwiftUI
+
+@main
+struct MenuPandaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MenuPandaTests/MenuPandaTests.swift
@@ -0,0 +1,16 @@
+//
+//  MenuPandaTests.swift
+//  MenuPandaTests
+//
+//  Created by Saurabh Dutta on 17/05/25.
+//
+
+import Testing
+
+struct MenuPandaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
